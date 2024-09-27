da10c4eb9734fde26d8563c7047f56f5047ffc8e
diyach868 <155082615+diyach868@users.noreply.github.com>
2024-09-27 19:08:19 +0545

    Update theme.dart

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:sajilo_charge/src/utils/theme/widget_themes/text_themes.dart';
 
 class TAppTheme {
-  static ThemeData lightTheme = ThemeData(brightness: Brightness.light);
-  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light,
+      textTheme: TTextTheme.lightTextTheme,
+      elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom())
+  );
+  static ThemeData darkTheme = ThemeData(
+      brightness: Brightness.dark,
+      textTheme: TTextTheme.darkTextTheme,
+      elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom())
+  );
+
 }
